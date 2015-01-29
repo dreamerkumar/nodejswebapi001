ad1900ddc468f99e69efd221c210490731d52dc2
unknown <1019263@1019263WIN7DT.jda.corp.local>
2015-01-29 16:54:27 -0500

    Fixing issues with passing the port number that the app should run on

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-
-require("./module")(process.argv[2]);
+console.log('The port number for the environment is ' + process.env.PORT);
+console.log('The port number supplied is ' + process.argv[2]);
+var chosenPort = process.env.PORT | process.argv[2] | 3000;
+console.log('Attempting to run on port ' + chosenPort);
+require("./module")(chosenPort);
 
