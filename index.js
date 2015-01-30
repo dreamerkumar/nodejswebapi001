fb971f27a7ae7a82f1ef0b8e2f32662f08af1557
Vishal Kumar <vishalkumar@Vishals-MBP.home>
2015-01-29 19:08:00 -0500

    fixed problem with port selection logic

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log('The port number for the environment is ' + process.env.PORT);
 console.log('The port number supplied is ' + process.argv[2]);
-var chosenPort = process.env.PORT | process.argv[2] | 3000;
+var chosenPort = process.env.PORT || process.argv[2] || 3000;
 console.log('Attempting to run on port ' + chosenPort);
 require("./module")(chosenPort);
 
